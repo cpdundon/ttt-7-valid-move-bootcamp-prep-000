c4bef876504a1b7e8d6b866debcdd73ebe8addd6
Chris Dundon <chris.dundon@gmail.com>
2018-03-18 03:18:43 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,3 +2,18 @@
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+def position_taken?(board, idx)
+  len = board.length
+  if len == 0
+    return false
+  end
+  
+  out = board[idx]
+  out_strip = out.strip
+  if out_strip == "X" || out_strip == "O"
+    return true
+  else
+    return false
+  end
+end
