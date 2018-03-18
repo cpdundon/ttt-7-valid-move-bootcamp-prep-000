40dc65d82785b17a4a43103b1e5715b6bac9c7b9
Chris Dundon <chris.dundon@gmail.com>
2018-03-18 03:23:01 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,9 @@
 # code your #valid_move? method here
 def valid_move?(board, idx)
+  good_idx = idx.between(0, 8)
+  good_position = position_taken?(board, idx)
   
+  good_idx && good_position 
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
